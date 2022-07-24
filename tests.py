cb744ed5da9fe6b62ceddf4a66b9b20a91676e2f
Vitaliy Levchenkov <heagekwhat@yandex.ru>
2022-07-24 13:53:58 +0300

    Done

--- tests.py
+++ tests.py
@@ -1,24 +1,88 @@
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
+    def test__init__books_rating(self):  # Проверка создания пустого словаря.
         collector = BooksCollector()
+        assert collector.books_rating == {}
 
-        # добавляем две книги
+    def test__init__favorites(self):  # Проверка создания пустого списка.
+        collector = BooksCollector()
+        assert collector.favorites == []
+
+    def test_add_new_book_add_two_books(self):  # Добавление двух книг.
+        collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_one_book_twice(self):  # Нельзя добавить одну и ту же книгу дважды.
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        assert len(collector.get_books_rating()) == 1
+
+    def test_set_book_rating_which_is_not_in_the_list(self):  # Нельзя выставить рейтинг книге, которой нет в списке.
+        collector = BooksCollector()
+        collector.set_book_rating('Гордость и предубеждение и зомби', 8)
+        assert len(collector.books_rating) == 0
+
+    def test_set_book_rating_less_than_1(self):  # Нельзя выставить рейтинг меньше 1.
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.set_book_rating('Гордость и предубеждение и зомби', 0)
+        assert collector.books_rating['Гордость и предубеждение и зомби'] != 0
+
+    def test_set_book_rating_more_than_10(self):  # Нельзя выставить рейтинг больше 10.
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.set_book_rating('Гордость и предубеждение и зомби', 11)
+        assert collector.books_rating['Гордость и предубеждение и зомби'] < 10
+
+    def test_get_book_rating_at_not_added_book_not_rating(self):  # У не добавленной книги нет рейтинга.
+        collector = BooksCollector()
+        assert collector.get_book_rating('Гордость и предубеждение и зомби') == None
+
+    def test_get_book_rating_added_book_has_a_default_rating_of_1(self):  # У добавленной книги рейтинг по умиолчанию 1.
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        assert collector.get_book_rating('Гордость и предубеждение и зомби') == 1
+
+    def test_get_books_with_specific_rating(self):  # Вывод списка книг с определенным рейтингом.
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        assert collector.get_books_with_specific_rating(1) == ['Гордость и предубеждение и зомби']
+
+    def test_get_books_with_specific_rating_incorrect_rating(self):  # Вывод списка книг с неккоретным рейтингом.
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.set_book_rating('Гордость и предубеждение и зомби', 6)
+        assert collector.get_books_with_specific_rating(8) == []
+
+    def test_get_books_rating_efficiency(self):  # Проверка метода get_books_rating.
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        assert collector.get_books_rating() == {'Гордость и предубеждение и зомби': 1}
+
+    def test_get_books_rating_efficiency_no_add_book(self):  # Проверка метода get_books_rating не добавляя книг.
+        collector = BooksCollector()
+        assert collector.get_books_rating() == {}
+
+    def test_add_book_in_favorites(self):  # Добавление книг в избранное.
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_book_in_favorites('Гордость и предубеждение и зомби')
+        assert collector.favorites == ['Гордость и предубеждение и зомби']
+
+    def test_delete_book_from_favorites(self):  # Удаление книги из избранного.
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_book_in_favorites('Гордость и предубеждение и зомби')
+        assert collector.delete_book_from_favorites('Гордость и предубеждение и зомби') == None
+
+    def test_get_list_of_favorites_books_1(self):  # Получение список Избранных книг.
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_book_in_favorites('Гордость и предубеждение и зомби')
+        assert collector.get_list_of_favorites_books() == ['Гордость и предубеждение и зомби']
